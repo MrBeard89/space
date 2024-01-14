b708736c59c92bd1368eb08b6ff4e6e1328d699c
Norbert Kovács <112707533+MrBeard89@users.noreply.github.com>
2024-01-14 17:29:59 +0100

    Navbar mobile finished

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { Home } from './components/pages/Home'
 
 import './App.css'
 import { Navbar } from './components/navbar/Navbar'
+import { Crew } from './components/pages/Crew'
+import { Destination } from './components/pages/Destination'
+import { Technology } from './components/pages/Technology'
 
 function App() {
   return (
@@ -11,6 +14,10 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' element={<Home />}></Route>
+          <Route path='/home' element={<Home />}></Route>
+          <Route path='/crew' element={<Crew />}></Route>
+          <Route path='/destination' element={<Destination />}></Route>
+          <Route path='/technology' element={<Technology />}></Route>
         </Routes>
       </BrowserRouter>
     </>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,8 @@
 import CloseIcon from '@mui/icons-material/Close'
 import MenuIcon from '@mui/icons-material/Menu'
-import { Box } from '@mui/material'
+import { Box, List, ListItem, ListItemText } from '@mui/material'
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 export const Navbar = () => {
   const [openBtn, setOpenBtn] = useState(false)
@@ -33,7 +34,10 @@ export const Navbar = () => {
   return (
     <Box className='navbar_wrapper' sx={navbarWrapper}>
       <Box className='navbar_container' sx={navbarContainer}>
-        <img src='../src/assets/shared/logo.svg' alt='logo' />
+        <Link to='/home'>
+          <img src='../src/assets/shared/logo.svg' alt='logo' />
+        </Link>
+
         <Box onClick={handleOpenBtn}>
           {openBtn == false ? (
             <MenuIcon
@@ -41,10 +45,11 @@ export const Navbar = () => {
                 color: '#D0D6F9',
                 fontSize: '2.5rem',
                 zIndex: '4',
+                cursor: 'pointer',
               }}
             />
           ) : (
-            <CloseIcon sx={{ color: '#D0D6F9', fontSize: '2.5rem' }} />
+            ''
           )}
         </Box>
 
@@ -55,21 +60,293 @@ export const Navbar = () => {
             position: 'absolute',
             bgcolor: 'rgba(255,255,255, 0.1)',
             top: '0',
-            right: openBtn ? '0' : '-15rem',
+            right: openBtn ? '0' : '-17rem',
             zIndex: '3',
             transition: 'ease-in-out 0.3s',
             backdropFilter: 'blur(25px)',
+            display: 'flex',
+            flexDirection: 'column-reverse',
+            alignItems: 'flex-end',
+            justifyContent: 'flex-end',
           }}
         >
-          <ul>
-            <li>00 Home</li>
-            <li>01 Destination</li>
-            <li>02 Crew</li>
-            <li>03 Technology</li>
-          </ul>
+          {/* Links */}
+          <List
+            className='navlink_container'
+            sx={[
+              {
+                listStyle: 'none',
+                color: '#fff',
+                textTransform: 'uppercase',
+                display: 'flex',
+                flexDirection: 'column',
+                mt: '4.5rem',
+              },
+            ]}
+          >
+            {/* Home */}
+            <Link to='/home' style={{ textDecoration: 'none' }} onClick={() => setOpenBtn(false)}>
+              <ListItem
+                sx={[
+                  {
+                    width: '254px',
+                    height: '3rem',
+                    display: 'flex',
+                    alignItems: 'center',
+                    position: 'relative',
+                    color: '#fff',
+                  },
+
+                  {
+                    '&:hover': {
+                      cursor: 'pointer',
+                      '&::after': {
+                        content: '""',
+                        width: '5px',
+                        height: '3rem',
+                        position: 'absolute',
+                        right: '0',
+                        bgcolor: 'gray',
+                      },
+                    },
+                    '&:active': {
+                      '&::after': {
+                        content: '""',
+                        width: '5px',
+                        height: '3rem',
+                        position: 'absolute',
+                        right: '0',
+                        bgcolor: 'white',
+                      },
+                    },
+                  },
+                ]}
+              >
+                <ListItemText sx={{ position: 'relative' }}>
+                  <span
+                    style={{
+                      fontWeight: '500',
+                      position: 'relative',
+                      left: '1.5rem',
+                      fontSize: '1.3rem',
+                    }}
+                  >
+                    00
+                  </span>
+                  <span
+                    style={{
+                      fontSize: '1.3rem',
+                      position: 'relative',
+                      left: '2.6rem',
+                      letterSpacing: '0.1rem',
+                    }}
+                  >
+                    Home
+                  </span>
+                </ListItemText>
+              </ListItem>
+            </Link>
+
+            {/* Destination */}
+            <Link
+              to='/destination'
+              style={{ textDecoration: 'none' }}
+              onClick={() => setOpenBtn(false)}
+            >
+              <ListItem
+                sx={[
+                  {
+                    width: '254px',
+                    height: '3rem',
+                    display: 'flex',
+                    alignItems: 'center',
+                    position: 'relative',
+                    color: '#fff',
+                  },
+
+                  {
+                    '&:hover': {
+                      cursor: 'pointer',
+                      '&::after': {
+                        content: '""',
+                        width: '5px',
+                        height: '3rem',
+                        position: 'absolute',
+                        right: '0',
+                        bgcolor: 'gray',
+                      },
+                    },
+                    '&:active': {
+                      '&::after': {
+                        content: '""',
+                        width: '5px',
+                        height: '3rem',
+                        position: 'absolute',
+                        right: '0',
+                        bgcolor: 'white',
+                      },
+                    },
+                  },
+                ]}
+              >
+                <ListItemText sx={{ position: 'relative' }}>
+                  <span
+                    style={{
+                      position: 'relative',
+                      left: '1.5rem',
+                      fontWeight: '500',
+                      fontSize: '1.3rem',
+                    }}
+                  >
+                    01
+                  </span>
+                  <span
+                    style={{
+                      fontSize: '1.3rem',
+                      position: 'relative',
+                      left: '3rem',
+                      letterSpacing: '0.1rem',
+                    }}
+                  >
+                    Destination
+                  </span>
+                </ListItemText>
+              </ListItem>
+            </Link>
+
+            {/* Crew */}
+            <Link to='/crew' style={{ textDecoration: 'none' }} onClick={() => setOpenBtn(false)}>
+              <ListItem
+                sx={[
+                  {
+                    width: '254px',
+                    height: '3rem',
+                    display: 'flex',
+                    alignItems: 'center',
+                    position: 'relative',
+                    color: '#fff',
+                  },
+
+                  {
+                    '&:hover': {
+                      cursor: 'pointer',
+                      '&::after': {
+                        content: '""',
+                        width: '5px',
+                        height: '3rem',
+                        position: 'absolute',
+                        right: '0',
+                        bgcolor: 'gray',
+                      },
+                    },
+                    '&:active': {
+                      '&::after': {
+                        content: '""',
+                        width: '5px',
+                        height: '3rem',
+                        position: 'absolute',
+                        right: '0',
+                        bgcolor: 'white',
+                      },
+                    },
+                  },
+                ]}
+              >
+                <ListItemText sx={{ position: 'relative' }}>
+                  <span
+                    style={{
+                      fontWeight: '500',
+                      position: 'relative',
+                      left: '1.5rem',
+                      fontSize: '1.3rem',
+                    }}
+                  >
+                    02
+                  </span>
+                  <span
+                    style={{
+                      fontSize: '1.3rem',
+                      position: 'relative',
+                      left: '2.6rem',
+                      letterSpacing: '0.1rem',
+                    }}
+                  >
+                    Crew
+                  </span>
+                </ListItemText>
+              </ListItem>
+            </Link>
+
+            {/* Technology */}
+            <Link
+              to='/technology'
+              style={{ textDecoration: 'none' }}
+              onClick={() => setOpenBtn(false)}
+            >
+              <ListItem
+                sx={[
+                  {
+                    width: '254px',
+                    height: '3rem',
+                    display: 'flex',
+                    alignItems: 'center',
+                    position: 'relative',
+                    color: '#fff',
+                  },
+
+                  {
+                    '&:hover': {
+                      cursor: 'pointer',
+                      '&::after': {
+                        content: '""',
+                        width: '5px',
+                        height: '3rem',
+                        position: 'absolute',
+                        right: '0',
+                        bgcolor: 'gray',
+                      },
+                    },
+                    '&:active': {
+                      '&::after': {
+                        content: '""',
+                        width: '5px',
+                        height: '3rem',
+                        position: 'absolute',
+                        right: '0',
+                        bgcolor: 'white',
+                      },
+                    },
+                  },
+                ]}
+              >
+                <ListItemText sx={{ position: 'relative' }}>
+                  <span
+                    style={{
+                      fontWeight: '500',
+                      position: 'relative',
+                      left: '1.5rem',
+                      fontSize: '1.3rem',
+                    }}
+                  >
+                    03
+                  </span>
+                  <span
+                    style={{
+                      fontSize: '1.3rem',
+                      position: 'relative',
+                      left: '2.6rem',
+                      letterSpacing: '0.1rem',
+                    }}
+                  >
+                    Technology
+                  </span>
+                </ListItemText>
+              </ListItem>
+            </Link>
+          </List>
 
           <CloseIcon
-            sx={{ color: '#D0D6F9', fontSize: '2.5rem' }}
+            sx={{ color: '#D0D6F9', fontSize: '2.5rem', p: '1rem', cursor: 'pointer' }}
             onClick={() => setOpenBtn(false)}
           />
         </Box>
